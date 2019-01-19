0c4c5b4c2b39533fc20b91a832c0a26107054a98
PotatoParser <wilsonqnguyen@gmail.com>
2019-01-18 18:14:06 -0800

    Added download urls

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const cp = require("child_process");
 const OS = process.platform;
 const BIT = process.arch;
 if (!(BIT === "x32" || BIT === "x64")) console.error(new Error("CPU architecture not supported"));
+const OSURL = {
+	linux: {
+		x32: "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz",
+		x64: "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz"
+	},
+	darwin: {
+		x64: "https://ffmpeg.zeranoe.com/builds/macos64/static/ffmpeg-latest-macos64-static.zip",
+	},
+	win32: {
+		x32: "https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.zip",
+		x64: "https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip"
+	}
+}
 const allOS = {
 	linux: {
 		x32: __dirname + "/ffmpeg/linux32/ffmpeg",
@@ -17,6 +30,7 @@ const allOS = {
 }
 if (allOS[OS] === undefined) console.error(new Error("OS not supporteds!"));
 if (allOS[OS][BIT] === undefined) console.error(new Error("Invalid OS and CPU architecture!"));
+
 module.exports = class FFMPEG {
 	static async run(cmd){
 		var temp = await new Promise((resolve, reject)=>{
