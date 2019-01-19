ae2afeeb560c3b7b2590ba13022f7dfc92ef5e19
PotatoParser <wilsonqnguyen@gmail.com>
2019-01-19 15:27:46 -0800

    Abstraction layer

---
+++ verify.js
@@ -0,0 +1,38 @@
+const OS = process.platform;
+const BIT = process.arch;
+const chalk = require("chalk");
+function error(text){
+	console.log(chalk.bold.red("ERROR! ") + chalk.white("[ffmpeg-cli]") + " " + new Error(text));
+}
+if (!(BIT === "x32" || BIT === "x64")) error("CPU architecture not supported");
+const allOS = {
+	linux: {
+		x32: {
+			url: "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz",
+			path: __dirname + "/ffmpeg/linuxx32/ffmpeg"
+		},
+		x64: {
+			url: "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz",
+			path: __dirname + "/ffmpeg/linuxx64/ffmpeg"
+		}
+	},
+	darwin: {
+		x64: {
+			url: "https://ffmpeg.zeranoe.com/builds/macos64/static/ffmpeg-latest-macos64-static.zip",
+			path: __dirname + "/ffmpeg/darwinx64/bin/ffmpeg"
+		}
+	},
+	win32: {
+		x32: {
+			url: "https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.zip",
+			path: __dirname + "/ffmpeg/win32x32/bin/ffmpeg.exe"
+		},
+		x64: {
+			url: "https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip",
+			path: __dirname + "/ffmpeg/win32x64/bin/ffmpeg.exe"
+		}
+	}
+}
+if (allOS[OS] === undefined) error("OS not supporteds!");
+if (allOS[OS][BIT] === undefined) error("Invalid OS and CPU architecture!");
+module.exports = allOS[OS][BIT];
