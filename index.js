a339ad80cef56f3e99f97796d12a807cdbe41e40
PotatoParser <wilsonqnguyen@gmail.com>
2019-01-19 09:56:02 -0800

    Removed extra lines

--- index.js
+++ index.js
@@ -3,32 +3,6 @@ const OS = process.platform;
 const BIT = process.arch;
 const fs = require("fs");
 if (!(BIT === "x32" || BIT === "x64")) console.error(new Error("CPU architecture not supported"));
-const OSURL = {
-	linux: {
-		x32: "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz",
-		x64: "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz"
-	},
-	darwin: {
-		x64: "https://ffmpeg.zeranoe.com/builds/macos64/static/ffmpeg-latest-macos64-static.zip",
-	},
-	win32: {
-		x32: "https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.zip",
-		x64: "https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip"
-	}
-}
-/*const allOS = {
-	linux: {
-		x32: __dirname + "/ffmpeg/linux32/ffmpeg",
-		x64: __dirname + "/ffmpeg/linux64/ffmpeg"
-	},
-	darwin: {
-		x64: __dirname + "/ffmpeg/mac64/bin/ffmpeg",
-	},
-	win32: {
-		x32: __dirname + "/ffmpeg/win32/bin/ffmpeg",
-		x64: __dirname + "/ffmpeg/win64/bin/ffmpeg"
-	}
-}*/
 const allOS = {
 	linux: {
 		x32: {
