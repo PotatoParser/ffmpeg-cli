3ede9a119cd3aaf01c5ba2e3278fd11fcb4349ca
PotatoParser <wilsonqnguyen@gmail.com>
2019-01-30 12:28:31 -0800

    For testing with Travis Windows

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const ffmpeg = require("./index");
+console.log(__dirname);
 ffmpeg.run("-version").catch((result)=>console.log(result)).then((result)=>console.log(result)); // Asynchronous
 ffmpeg.run("error").then((result)=>console.log(result)).catch((result)=>console.log("Test ERROR")); // Asynchronous
 console.log(ffmpeg.runSync("-version"));
