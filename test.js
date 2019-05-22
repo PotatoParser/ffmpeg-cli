6f45373ea4958d60a6bbfa376925965de95c6978
PotatoParser <wilsonqnguyen@gmail.com>
2019-05-21 21:19:04 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const ffmpeg = require("./index");
 console.log(__dirname);
+console.log(ffmpeg.path);
 ffmpeg.run("-version").catch((result)=>console.log(result)).then((result)=>console.log(result)); // Asynchronous
 ffmpeg.run("error").then((result)=>console.log(result)).catch((result)=>console.log("Test ERROR")); // Asynchronous
 console.log(ffmpeg.runSync("-version"));
