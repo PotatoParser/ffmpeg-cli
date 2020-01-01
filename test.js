91f93ace6ba7e2bb6a92ccd5692295f43d11d66a
PotatoParser <wilsonqnguyen@gmail.com>
2020-01-01 00:58:04 -0800

    Make test more useful

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 const ffmpeg = require('./lib/index');
+ffmpeg.forceDownload();
 console.log(__dirname);
 console.log(ffmpeg.path);
 ffmpeg.run('-version').catch((result)=>console.log(result)).then((result)=>console.log(result)); // Asynchronous
 ffmpeg.run('error').then((result)=>console.log(result)).catch((result)=>console.log('Test ERROR')); // Asynchronous
-console.log(ffmpeg.runSync('-version'));
+try {
+	console.log(ffmpeg.runSync('-version'));
+} catch(e) {
+	console.log(e.stdout, e.stderr, e.pid, e.signal, e.status, e.output);
+    console.error(e);
+}
