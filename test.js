8144ddeddf6e4a1b3c265e19d2bddec77ae6e065
PotatoParser <wilsonqnguyen@gmail.com>
2020-01-01 01:11:09 -0800

    Remove Windows travis testing

--- test.js
+++ test.js
@@ -4,9 +4,4 @@ console.log(__dirname);
 console.log(ffmpeg.path);
 ffmpeg.run('-version').catch((result)=>console.log(result)).then((result)=>console.log(result)); // Asynchronous
 ffmpeg.run('error').then((result)=>console.log(result)).catch((result)=>console.log('Test ERROR')); // Asynchronous
-try {
-	console.log(ffmpeg.runSync('-version'));
-} catch(e) {
-	console.log(e.stdout, e.stderr, e.pid, e.signal, e.status, e.output);
-    console.error(e);
-}
+console.log(ffmpeg.runSync('-version'));
