bdc8c97e84fd52d771a5aeffe345894ab080cf00
PotatoParser <45542237+PotatoParser@users.noreply.github.com>
2020-09-26 22:14:38 -0700

    Lint download apis (#12)
    * Lint & Add new download URLs
    
    * Add precommit
    
    * 2.7.0

--- test.js
+++ test.js
@@ -2,6 +2,10 @@ const ffmpeg = require('./lib/index');
 ffmpeg.forceDownload();
 console.log(__dirname);
 console.log(ffmpeg.path);
-ffmpeg.run('-version').catch((result)=>console.log(result)).then((result)=>console.log(result)); // Asynchronous
-ffmpeg.run('error').then((result)=>console.log(result)).catch((result)=>console.log('Test ERROR')); // Asynchronous
+ffmpeg.run('-version')
+	.catch(result => console.log(result))
+	.then(result => console.log(result)); // Asynchronous
+ffmpeg.run('error')
+	.then(result => console.log(result))
+	.catch(() => console.log('Test ERROR')); // Asynchronous
 console.log(ffmpeg.runSync('-version'));
